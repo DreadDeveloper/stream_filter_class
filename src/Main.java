07988d766f012721d807d3774f5c4b6960110157
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-01 18:42:53 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		List<Student> studentList = new ArrayList<>();
+		studentList.add(new Student(1, "AS", 32.0f));
+		studentList.add(new Student(2, "SK", 99.0f));
+		studentList.add(new Student(3, "NA", 90.0f));
+		studentList.add(new Student(4, "AA", 100.0f));
+		studentList.add(new Student(5, "DB", 101.0f));
+		studentList.add(new Student(6, "AK", 99.0f));
+		studentList.add(new Student(7, "AP", 98.0f));
+		studentList.add(new Student(8, "P", 97.0f));
+		studentList.add(new Student(9, "AR", 96.0f));
+		studentList.add(new Student(10, "JA", 95.0f));
+		studentList.add(new Student(11, "AK", 94.0f));
+		//Using forEach
+		studentList.stream().filter(n -> n.getRollNo() > 5).forEach(n -> System.out.println(n.getRollNo() + "  " + n.getStudentName() + ' ' + n.getAvgMarks()));
+		//Using Collectors
+		List<Student> rollList = studentList.stream().filter(n -> n.getRollNo() > 5).collect(Collectors.toList());
+		for(Student n : rollList)
+		{
+			System.out.println(n.getRollNo() + " " + n.getStudentName() + " " + n.getAvgMarks());
+		}
+	}
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Student {
+public class Student
+{
+	private final int rollNo;
+	private final String studentName;
+	private final float avgMarks;
+	Student(int rollNo, String studentName, float avgMarks)
+	{
+		this.rollNo = rollNo;
+		this.studentName = studentName;
+		this.avgMarks = avgMarks;
+	}
+	public int getRollNo()
+	{
+		return rollNo;
+	}
+	public String getStudentName()
+	{
+		return studentName;
+	}
+	public float getAvgMarks()
+	{
+		return avgMarks;
+	}
 }
